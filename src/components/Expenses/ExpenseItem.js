c44fc0ee9596df610f594be78d0223b06acb8fd8
azhim <ariefazhim@gmail.com>
2022-02-25 14:56:05 +0800

    adjustments

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,7 +4,7 @@ import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
 import React from 'react';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
