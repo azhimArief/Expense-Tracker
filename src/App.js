7b53622f01bdd9801555474cfc51543e91638744
azhim <ariefazhim@gmail.com>
2022-02-26 13:58:01 +0800

    added newExpense for inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Expenses from "./components/Expenses/Expenses"; 
 import React from 'react';
+import NewExpense from "./components/NewExpense/NewExpense";
 
 //arrow function
 const App = () => {
@@ -36,7 +37,7 @@ const App = () => {
   
   return (
     <div>
-      <h2>Let's get started!</h2> 
+      <NewExpense></NewExpense>
       <Expenses items= {expenses}></Expenses>
     </div>
   ); 
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,16 +2,26 @@
 import "./ExpenseItem.css";
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
-import React from 'react';
+import React, {useState} from 'react';
 
 const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title);
+
+  //function clickHandler
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  }
+  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick = {clickHandler}>Change Title</button>
     </Card>
   );
 }
